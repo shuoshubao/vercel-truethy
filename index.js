dc1d7e748d4e9a3029d771c577a445c63ac684e3
shuoshubao <shuoshubao@gmail.com>
2023-07-21 14:05:29 +0800

    feat: koa router

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const Koa = require('koa')
+const Router = require('@koa/router')
+const cors = require('@koa/cors')
 
 const app = new Koa()
+const router = new Router()
+
+app.use(cors())
+
+router.get('/api/demo', (ctx, next) => {
+  ctx.body = {
+    code: 0,
+    message: '',
+    data: 'demo'
+  }
+})
+
+app.use(router.routes())
+
+app.use(router.allowedMethods())
 
 app.use(async ctx => {
   ctx.body = 'hello koa.js'
