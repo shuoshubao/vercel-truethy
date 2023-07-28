30d55fc45c6dcbe59d7a60b1368a4a312d55dd01
shuoshubao <shuoshubao@gmail.com>
2023-07-28 13:38:40 +0800

    feat: files

--- api/env/index.js
+++ api/env/index.js
@@ -1,18 +1,27 @@
+const { glob } = require('glob');
+
 module.exports = {
   method: 'get',
   description: '获取环境变量',
   args: {
     type: 'null'
   },
-  middleware: ctx => {
+  middleware: async ctx => {
     const timestap = Date.now();
+
+    const files = await glob('*/**/*.{js,jsx,css,less,scss,sass,md}', {
+      ignore: 'node_modules/**',
+      nodir: true
+    });
+
     ctx.body = {
       code: 0,
       message: '',
       time: Date.now() - timestap,
       data: {
         cwd: process.cwd(),
-        env: process.env
+        env: process.env,
+        files: files.sort()
       }
     };
   }
