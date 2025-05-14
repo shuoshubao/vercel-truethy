f86ec849ee949383edf7124a5eb9211955fe3330
shuoshubao <shuoshubao@gmail.com>
2025-05-14 16:41:56 +0800

    ci: github actions

--- scripts/router.js
+++ scripts/router.js
@@ -1,4 +1,5 @@
 const { writeFileSync } = require('fs');
+const { ensureFileSync } = require('fs-extra');
 const { resolve, relative } = require('path');
 const glob = require('glob');
 
@@ -7,5 +8,7 @@ const cwd = process.cwd();
 const Routerfiles = glob.sync(resolve(cwd, 'api/**/*.js')).map(v => {
   return relative(cwd, v).replace('.js', '');
 });
+
+ensureFileSync('dist/routerConfig.json');
 
 writeFileSync('dist/routerConfig.json', JSON.stringify(Routerfiles) + '\n');
